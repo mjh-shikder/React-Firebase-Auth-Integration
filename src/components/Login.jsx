58ef104e7cabfee814021025d91519af0ee2bd0b
mjh-shikder <mjhsikder@gmail.com>
2025-10-27 15:22:47 +0600

    login page and register page added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
     return (
-        <div>
-            <h1 className='text-3xl text-center font-semibold text-green-700 my-24'> Login Page</h1>
-        </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-20">
+      <div className="card-body">
+        <h1 className="text-3xl font-bold text-center">Login</h1>
+        <form>
+          <fieldset className="fieldset">
+            {/* Email Feild */}
+            <label className="label">Email</label>
+            <input
+              type="email"
+              className="input"
+              name="email"
+              placeholder="Email"
+            />
+            {/* Password Feild */}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              className="input"
+              name="password"
+              placeholder="Password"
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+              </form>
+              <p>Don't Have an Accout? Then <Link className="text-blue-500 hover:text-blue-800 hover:font-semibold" to={'/register'}>Register</Link> </p>
+      </div>
+    </div>
     );
 };
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
-    return (
-        <div>
-            <h1 className='text-3xl text-center font-semibold text-green-700 my-24'> Register.jsx Page</h1>
-        </div>
-    );
+  return (
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-20">
+      <div className="card-body">
+        <h1 className="text-3xl font-bold text-center">Register Now!</h1>
+        <form>
+          <fieldset className="fieldset">
+            {/* Email Feild */}
+            <label className="label">Email</label>
+            <input
+              type="email"
+              className="input"
+              name="email"
+              placeholder="Email"
+            />
+            {/* Password Feild */}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              className="input"
+              name="password"
+              placeholder="Password"
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Register</button>
+          </fieldset>
+              </form>
+              <p>Already Have an Accoutn? Then <Link className="text-blue-500 hover:text-blue-800 hover:font-semibold" to={'/login'}>Login</Link> </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
