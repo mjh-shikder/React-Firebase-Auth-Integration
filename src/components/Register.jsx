eef17c04d793eed7eba140eabf96d2bb3be2c5bf
mjh-shikder <mjhsikder@gmail.com>
2025-10-27 15:43:44 +0600

    register function added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 import { Link } from "react-router";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Firebase/firebase.init";
 
 const Register = () => {
+
+  const handleRegister = e => {
+    e.preventDefault()
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const name = e.target.name.value;
+    console.log(email, password, name);
+    
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+      console.log(result);
+      
+    })
+      .catch(error => {
+      console.log(error);
+      
+    })
+}
+
+
+
   return (
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-20">
       <div className="card-body">
         <h1 className="text-3xl font-bold text-center">Register Now!</h1>
-        <form>
+        <form onSubmit={handleRegister}>
           <fieldset className="fieldset">
+            {/* Name Feild */}
+            <label className="label">Name</label>
+            <input
+              type="text"
+              className="input"
+              name="name"
+              placeholder="Your Name"
+            />
             {/* Email Feild */}
             <label className="label">Email</label>
             <input
