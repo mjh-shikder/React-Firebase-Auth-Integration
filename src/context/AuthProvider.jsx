e1727e0b1486af5b0e62308b5eaa9f03388f157d
mjh-shikder <mjhsikder@gmail.com>
2025-11-04 23:09:17 +0600

    onAuthStateChanged added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../Firebase/firebase.init';
 
 
@@ -15,6 +15,17 @@ const AuthProvider = ({ children }) => {
     const signInUser = (email, passowrd) => {
         return signInWithEmailAndPassword(auth, email, passowrd )
     }
+    // Get current user info 
+    onAuthStateChanged(auth, (currentUser) => {
+        if (currentUser) {
+            console.log('inside observer: if', currentUser);
+        }
+        else {
+            console.log('inside observer: else', currentUser);
+            
+        }
+    })
+
    
     const authInfo = {
         createUser,
