ce03d84cfa7752fb87c36d3b3092536d37b5c5eb
mjh-shikder <mjhsikder@gmail.com>
2025-11-07 22:27:44 +0600

    Signin with google added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../Firebase/firebase.init';
 
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
@@ -16,6 +18,11 @@ const AuthProvider = ({ children }) => {
     const signInUser = (email, passowrd) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth, email, passowrd )
+    }
+
+    const signInWithGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider )
     }
 
     const signOutUser = () => {
@@ -63,6 +70,7 @@ const AuthProvider = ({ children }) => {
         loading,
         createUser,
         signInUser,
+        signInWithGoogle,
         signOutUser,
     }
 
